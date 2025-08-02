72da1f46eb49b744f771692bd6d40753b73ae7f6
Wesley <wesley.analistasystem@gmail.com>
2025-08-01 22:41:46 -0300

    Commit editando a parte do js do projeto no notebook

--- js-yugioh-assets/src/scripts/engine.js
+++ js-yugioh-assets/src/scripts/engine.js
@@ -18,8 +18,12 @@ const state = {
     },
 };
 
-const pathImages = ".src/assets/icons";
+const playerSides = {
+    player1: "player-field-card",
+    computer: "computer-field-card",
+}
 
+const pathImages = ".src/assets/icons";
 
 const cardData = [
     {
@@ -48,6 +52,18 @@ const cardData = [
      },
 ];
 
-function init() {}
+async function drawCards(cardNumbers, fieldSide) {
+    for (let i = 0; i < cardNumbers ;i++) {
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+        document.getElementById(fieldSide).appendChild(cardImage);
+    }
+}
+
+function init() {
+    drawCards(5, playerSides.player1);
+    drawCards(5, playerSides.computer);
+}
 
 init()
